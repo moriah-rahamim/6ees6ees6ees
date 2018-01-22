c4d1bcc1601da2786e126b40257fce2549699e66
Moriah Rahamim <moriahfaye@gmail.com>
2018-01-22 14:55:00 -0800

    implemented RetiredForagerBee in ES6 style

--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,21 @@
-class RetiredForagerBee {
-  // TODO..
+class RetiredForagerBee extends ForagerBee {
+
+  constructor() {
+    // call ForagerBee constructor
+    super();
+
+    this.age = 40;
+    this.job = 'gamble';
+    this.canFly = false;
+    this.color = 'grey';
+    // inherit .food, .eat() .treasureChest from superclasses
+  }
+
+  forage() {
+    return 'I am too old, let me play cards instead';
+  }
+
+  gamble(treasure) {
+    this.treasureChest.push(treasure);
+  }
 };
