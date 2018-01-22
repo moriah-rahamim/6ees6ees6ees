537cf4730979e6d53f571de614678301c01c3fd5
Moriah Rahamim <moriahfaye@gmail.com>
2018-01-22 14:47:02 -0800

    implemented Bee in ES6 style. plus minor style edit to Grub

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,12 @@
-class Bee {
-  // TODO..
+class Bee extends Grub {
+  
+  constructor() {
+    // call the Grub constructor
+    super();
+
+    this.age = 5;
+    this.color = 'yellow';
+    // inherit .food and .eat() from Grub and Grub.prototype
+    this.job = 'Keep on growing';
+  }
 };
--- src/Grub.js
+++ src/Grub.js
@@ -1,6 +1,6 @@
 class Grub {
 
-  constructor () {
+  constructor() {
     this.age = 0;
     this.color = 'pink';
     this.food = 'jelly';
