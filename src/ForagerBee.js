830f9217e5854d7da70b8a36d0694e47a435281c
Moriah Rahamim <moriahfaye@gmail.com>
2018-01-22 14:52:24 -0800

    implemented ForagerBee in ES6 style

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,17 @@
-class ForagerBee {
-  // TODO..
+class ForagerBee extends Bee {
+
+  constructor() {
+    // call Bee constructor function
+    super();
+
+    this.age = 10;
+    this.job = 'find pollen';
+    // .color, .food, .eat() inherited from Bee, Grub, Grub.prototype
+    this.canFly = true;
+    this.treasureChest = [];
+  }
+
+  forage(treasure) {
+    this.treasureChest.push(treasure);
+  }
 };
