b2bb9593deadb99fc59229ef6db478ee657b81d8
Moriah Rahamim <moriahfaye@gmail.com>
2018-01-22 14:49:44 -0800

    implemented HoneyMakerBee in ES6 style

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,20 @@
-class HoneyMakerBee {
-  // TODO..
+class HoneyMakerBee extends Bee {
+
+  constructor() {
+    // call Bee constructor function
+    super();
+
+    this.age = 10;
+    this.job = 'make honey';
+    // .color, .food, .eat() inherited from Bee, Grub, and Grub.prototype
+    this.honeyPot = 0;
+  }
+
+  makeHoney() {
+    this.honeyPot++;
+  }
+
+  giveHoney() {
+    this.honeyPot--;
+  }
 };
